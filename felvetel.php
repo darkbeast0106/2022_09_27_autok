eba36f9a93f81b3aca48dd905930498de0e6a3e4
Budaházi Bence <budahazi.bence@petrik.hu>
2022-09-27 19:51:15 +0200

    Navigáció elhelyezése

--- felvetel.php
+++ felvetel.php
@@ -11,81 +11,102 @@
 </head>
 
 <body>
-    <?php 
-    $uzemanyag_tipusok = [
-        'benzin' => "Benzin", 
-        'gazolaj' => "Gázolaj", 
-        'elektromos' => "Elektromos", 
-        'hibrid' => "Hibrid"
-    ];
-    ?>
-    <?php 
-    if (isset($_POST) && !empty($_POST)) {
-        $hiba = "";
-        if (!isset($_POST['rendszam']) || empty($_POST['rendszam'])) {
-            $hiba .= "Rendszám mező kitöltése kötelező. ";
-        }
-        if (!isset($_POST['marka']) || empty($_POST['marka'])) {
-            $hiba .= "Márka mező kitöltése kötelező. ";
-        }
-        if (!isset($_POST['modell']) || empty($_POST['modell'])) {
-            $hiba .= "Modell mező kitöltése kötelező. ";
-        }
-        if (!isset($_POST['gyartas_eve']) || empty($_POST['gyartas_eve'])) {
-            $hiba .= "Gyártás éve mező kitöltése kötelező. ";
-        } else if (!is_numeric($_POST['gyartas_eve']) || round($_POST['gyartas_eve']) != $_POST['gyartas_eve']){
-            $hiba .= "Gyártás éve csak egész szám lehet. ";
-        } else if ($_POST['gyartas_eve'] < 1900 || $_POST['gyartas_eve'] > date("Y")) {
-            $hiba .= "A gyártás éve 1900 és ". date("Y"). " közé kell hogy essen. ";
-        }
-        if (!isset($_POST['uzemanyag']) || empty($_POST['uzemanyag'])) {
-            $hiba .= "Üzemanyag típus mező kitöltése kötelező. ";
-        } else if (!in_array($_POST['uzemanyag'], array_keys($uzemanyag_tipusok))) {
-            $hiba .= "Üzemanyag típust a legördülő menüből válassza ki. ";
+    
+<nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">Autók</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="index.php">Autók listázása</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="felvetel.php">Autó felvétele</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <main class="container">
+        <?php
+        $uzemanyag_tipusok = [
+            'benzin' => "Benzin",
+            'gazolaj' => "Gázolaj",
+            'elektromos' => "Elektromos",
+            'hibrid' => "Hibrid"
+        ];
+        ?>
+        <?php
+        if (isset($_POST) && !empty($_POST)) {
+            $hiba = "";
+            if (!isset($_POST['rendszam']) || empty($_POST['rendszam'])) {
+                $hiba .= "Rendszám mező kitöltése kötelező. ";
+            }
+            if (!isset($_POST['marka']) || empty($_POST['marka'])) {
+                $hiba .= "Márka mező kitöltése kötelező. ";
+            }
+            if (!isset($_POST['modell']) || empty($_POST['modell'])) {
+                $hiba .= "Modell mező kitöltése kötelező. ";
+            }
+            if (!isset($_POST['gyartas_eve']) || empty($_POST['gyartas_eve'])) {
+                $hiba .= "Gyártás éve mező kitöltése kötelező. ";
+            } else if (!is_numeric($_POST['gyartas_eve']) || round($_POST['gyartas_eve']) != $_POST['gyartas_eve']){
+                $hiba .= "Gyártás éve csak egész szám lehet. ";
+            } else if ($_POST['gyartas_eve'] < 1900 || $_POST['gyartas_eve'] > date("Y")) {
+                $hiba .= "A gyártás éve 1900 és ". date("Y"). " közé kell hogy essen. ";
+            }
+            if (!isset($_POST['uzemanyag']) || empty($_POST['uzemanyag'])) {
+                $hiba .= "Üzemanyag típus mező kitöltése kötelező. ";
+            } else if (!in_array($_POST['uzemanyag'], array_keys($uzemanyag_tipusok))) {
+                $hiba .= "Üzemanyag típust a legördülő menüből válassza ki. ";
+            }
+            ?>
+            <?php if ($hiba == ""): ?>
+                <?php
+                $file = fopen("autok.csv", "a");
+                $sor = implode(";", $_POST) . PHP_EOL;
+                fwrite($file, $sor);
+                ?>
+                <p>Sikeres felvétel</p>
+            <?php else: ?>
+                <p><?php echo $hiba ?></p>
+            <?php endif; ?>
+        <?php
         }
         ?>
-        <?php if ($hiba == ""): ?>
-            <?php 
-            $file = fopen("autok.csv", "a");
-            $sor = implode(";", $_POST) . PHP_EOL;
-            fwrite($file, $sor);
-            ?>
-            <p>Sikeres felvétel</p>
-        <?php else: ?>
-            <p><?php echo $hiba ?></p>
-        <?php endif; ?>
-    <?php
-    }
-    ?>
-    <h1>Autók felvétele</h1>
-    <form action="felvetel.php" method="post" name="auto_felvetel">
-        <div>
-            <label for="rendszam_input">Rendszám</label>
-            <input type="text" id="rendszam_input" name="rendszam" placeholder="Rendszám">
-        </div>
-        <div>
-            <label for="marka_input">Márka</label>
-            <input type="text" id="marka_input" name="marka" placeholder="Márka">
-        </div>
-        <div>
-            <label for="modell_input">Modell</label>
-            <input type="text" id="modell_input" name="modell" placeholder="Modell">
-        </div>
-        <div>
-            <label for="gyartas_eve_input">Gyártás éve</label>
-            <input type="number" id="gyartas_eve_input" name="gyartas_eve" placeholder="Gyártás éve">
-        </div>
-        <div>
-            <label for="uzemanyag_input">Üzemanyag típus</label>
-            <select id="uzemanyag_input" name="uzemanyag">
-                <option value=""></option>
-                <?php foreach ($uzemanyag_tipusok as $key => $value): ?>
-                    <option value="<?php echo $key ?>"><?php echo $value ?></option>
-                <?php endforeach; ?>
-            </select>
-        </div>
-        <button type="submit">Felvétel</button>
-    </form>
+        <h1>Autók felvétele</h1>
+        <form action="felvetel.php" method="post" name="auto_felvetel">
+            <div>
+                <label for="rendszam_input">Rendszám</label>
+                <input type="text" id="rendszam_input" name="rendszam" placeholder="Rendszám">
+            </div>
+            <div>
+                <label for="marka_input">Márka</label>
+                <input type="text" id="marka_input" name="marka" placeholder="Márka">
+            </div>
+            <div>
+                <label for="modell_input">Modell</label>
+                <input type="text" id="modell_input" name="modell" placeholder="Modell">
+            </div>
+            <div>
+                <label for="gyartas_eve_input">Gyártás éve</label>
+                <input type="number" id="gyartas_eve_input" name="gyartas_eve" placeholder="Gyártás éve">
+            </div>
+            <div>
+                <label for="uzemanyag_input">Üzemanyag típus</label>
+                <select id="uzemanyag_input" name="uzemanyag">
+                    <option value=""></option>
+                    <?php foreach ($uzemanyag_tipusok as $key => $value): ?>
+                        <option value="<?php echo $key ?>"><?php echo $value ?></option>
+                    <?php endforeach; ?>
+                </select>
+            </div>
+            <button type="submit">Felvétel</button>
+        </form>
+    </main>
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -11,55 +11,74 @@
 </head>
 
 <body>
-    <table>
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Rendszám</th>
-                <th>Márka</th>
-                <th>Modell</th>
-                <th>Gyártás éve</th>
-                <th>Üzemanyag típus</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            $file = fopen("autok.csv", "r");
-
-            $uzemanyag_tipusok = [
-                'benzin' => "Benzin",
-                'gazolaj' => "Gázolaj",
-                'elektromos' => "Elektromos",
-                'hibrid' => "Hibrid"
-            ];
-            $i = 0;
-            ?>
-            <?php while ($sor = fgets($file)) : ?>
-                <?php
-                $i++;
-                $adatok = explode(';', trim($sor));
-                ?>
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">Autók</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Autók listázása</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="felvetel.php">Autó felvétele</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <main class="container">
+        <table>
+            <thead>
                 <tr>
-                    <td><?php echo $i ?></td>
-                    <td><?php echo $adatok[0] ?></td>
-                    <td><?php echo $adatok[1] ?></td>
-                    <td><?php echo $adatok[2] ?></td>
-                    <td><?php echo $adatok[3] ?></td>
-                    <td><?php echo $uzemanyag_tipusok[$adatok[4]]; ?></td>
+                    <th>#</th>
+                    <th>Rendszám</th>
+                    <th>Márka</th>
+                    <th>Modell</th>
+                    <th>Gyártás éve</th>
+                    <th>Üzemanyag típus</th>
                 </tr>
-            <?php endwhile; ?>
-        </tbody>
-        <tfoot>
-            <tr>
-                <th>#</th>
-                <th>Rendszám</th>
-                <th>Márka</th>
-                <th>Modell</th>
-                <th>Gyártás éve</th>
-                <th>Üzemanyag típus</th>
-            </tr>
-        </tfoot>
-    </table>
+            </thead>
+            <tbody>
+                <?php
+                $file = fopen("autok.csv", "r");
+                $uzemanyag_tipusok = [
+                    'benzin' => "Benzin",
+                    'gazolaj' => "Gázolaj",
+                    'elektromos' => "Elektromos",
+                    'hibrid' => "Hibrid"
+                ];
+                $i = 0;
+                ?>
+                <?php while ($sor = fgets($file)) : ?>
+                    <?php
+                    $i++;
+                    $adatok = explode(';', trim($sor));
+                    ?>
+                    <tr>
+                        <td><?php echo $i ?></td>
+                        <td><?php echo $adatok[0] ?></td>
+                        <td><?php echo $adatok[1] ?></td>
+                        <td><?php echo $adatok[2] ?></td>
+                        <td><?php echo $adatok[3] ?></td>
+                        <td><?php echo $uzemanyag_tipusok[$adatok[4]]; ?></td>
+                    </tr>
+                <?php endwhile; ?>
+            </tbody>
+            <tfoot>
+                <tr>
+                    <th>#</th>
+                    <th>Rendszám</th>
+                    <th>Márka</th>
+                    <th>Modell</th>
+                    <th>Gyártás éve</th>
+                    <th>Üzemanyag típus</th>
+                </tr>
+            </tfoot>
+        </table>
+    </main>
 </body>
 
 </html>
