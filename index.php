14c1b9ca6f8009248b86927f110cd09ea15eb649
Budaházi Bence <budahazi.bence@petrik.hu>
2022-09-27 19:44:47 +0200

    Autók kiíratása táblázatba

--- index.php
+++ index.php
@@ -1,12 +1,64 @@
 <!DOCTYPE html>
 <html lang="hu">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Autók</title>
-</head>
-<body>
     
+</head>
+
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Rendszám</th>
+                <th>Márka</th>
+                <th>Modell</th>
+                <th>Gyártás éve</th>
+                <th>Üzemanyag típus</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $file = fopen("autok.csv", "r");
+
+            $uzemanyag_tipusok = [
+                'benzin' => "Benzin",
+                'gazolaj' => "Gázolaj",
+                'elektromos' => "Elektromos",
+                'hibrid' => "Hibrid"
+            ];
+            $i = 0;
+            ?>
+            <?php while ($sor = fgets($file)) : ?>
+                <?php
+                $i++;
+                $adatok = explode(';', trim($sor));
+                ?>
+                <tr>
+                    <td><?php echo $i ?></td>
+                    <td><?php echo $adatok[0] ?></td>
+                    <td><?php echo $adatok[1] ?></td>
+                    <td><?php echo $adatok[2] ?></td>
+                    <td><?php echo $adatok[3] ?></td>
+                    <td><?php echo $uzemanyag_tipusok[$adatok[4]]; ?></td>
+                </tr>
+            <?php endwhile; ?>
+        </tbody>
+        <tfoot>
+            <tr>
+                <th>#</th>
+                <th>Rendszám</th>
+                <th>Márka</th>
+                <th>Modell</th>
+                <th>Gyártás éve</th>
+                <th>Üzemanyag típus</th>
+            </tr>
+        </tfoot>
+    </table>
 </body>
+
 </html>
