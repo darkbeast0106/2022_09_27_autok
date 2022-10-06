e148c0d9bb301ff93758e3c63098e08986e5aa18
Budaházi Bence <budahazi.bence@petrik.hu>
2022-10-06 20:34:55 +0200

    Refactor - oldalhoz tartozó script headben

--- index.php
+++ index.php
@@ -8,6 +8,18 @@
     <title>Autók</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+    <script>
+        addEventListener("DOMContentLoaded", () => {
+            const oldal = "<?php echo isset($_GET['oldal']) ? $_GET['oldal'] : "" ?>";
+            if (oldal.length > 0) {
+                const navLink = document.getElementById('nav_' + oldal);
+                navLink.classList.add('active');
+            } else {
+                const navLink = document.getElementById('nav_listaz');
+                navLink.classList.add('active');
+            }
+        });
+    </script>
 </head>
 
 <body>
@@ -45,16 +57,6 @@
         ?>
     </main>
 
-    <script>
-        const oldal = "<?php echo isset($_GET['oldal']) ? $_GET['oldal'] : "" ?>";
-        if (oldal.length > 0) {
-            const navLink = document.getElementById('nav_' + oldal);
-            navLink.classList.add('active');
-        } else {
-            const navLink = document.getElementById('nav_listaz');
-            navLink.classList.add('active');
-        }
-    </script>
 </body>
 
 </html>
